e349f70e6f9125bf5555e29ad7798c4961efac27
Ömer Kılıç <komer4542@gmail.com>
2023-12-10 03:05:27 +0300

    partials güncellendi

---
+++ views/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="card-footer text-body-secondary">
+    <center>
+        <p>Design by <b style="color: black;">ÖMER KILIÇ </b> | &copy; 2023</p>
+    </center>
+</div>
+
+</html>
---
+++ views/partials/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Anasayfa</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="ogrenciEkle.php">Öğrenci Ekle</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ogrenciBilgisi.php">Öğrenci Bilgisi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bolumler.php">Bölümler</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ogretmenler.php">Öğretmenler</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dersler.php">Dersler</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
